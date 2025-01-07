ba13b5c60f4eab98167298eef038a5dbec71a30b
agung <agungpermadi9@gmail.com>
2025-01-07 17:30:59 +0700

    skema migrasi versi

---
+++ golang-restful-api/db/migrations/20250107101630_create_table_third.up.sql
@@ -0,0 +1,5 @@
+create table third(
+                         id INT NOT NULL AUTO_INCREMENT,
+                         name VARCHAR(200)NOT NULL ,
+                         PRIMARY KEY (id)
+)ENGINE =InnoDB;
